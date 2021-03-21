4ec6fe88b8b53ce34de03b8fed6aca16900a9881
igort <igort@192.168.15.8>
2021-03-21 16:04:04 -0300

    Todos os menus do código

---
+++ Trabalho3/src/converter/Menu.java
@@ -0,0 +1,131 @@
+package converter;
+
+public class Menu {
+
+	// Abaixo consta o menu principal que aparecerá assim que iniciar o código
+	
+		public void menuPrincipal()
+		{
+			System.out.println("");
+			System.out.println("1- Comprimento");
+			System.out.println("2- Area");
+			System.out.println("3- Peso");
+			System.out.println("4- Volume");
+			System.out.println("5- Temperatura");
+			System.out.println("6- Tempo");
+			System.out.println("7- Velocidade");
+			System.out.println("8- Dados");
+			System.out.println("");
+		}
+		// Abaixo consta o menu do campo COMPRIMENTO contendo as unidades de medida
+		
+		public void menuComprimento()
+		{
+			System.out.println("");
+			System.out.println("1- Milimetro");
+			System.out.println("2- Centimetro");
+			System.out.println("3- Decimetro");
+			System.out.println("4- Metro");
+			System.out.println("5- Quilometro");
+			System.out.println("6- Milha");
+			System.out.println("7- Pes");
+			System.out.println("");
+		}
+		// Abaixo consta o menu do campo AREA contendo as unidades de medida
+		public void menuArea()
+		{
+			System.out.println("");
+			System.out.println("1- Milimetro quadrado");
+			System.out.println("2- Centimetro quadrado");
+			System.out.println("3- Decimetro quadrado");
+			System.out.println("4- Metro quadrado");
+			System.out.println("5- Quilometro quadrado");
+			System.out.println("6- Milha quadrada");
+			System.out.println("7- Pes quadrados");
+			System.out.println("");
+		}
+		// Abaixo consta o menu do campo PESO contendo as unidades de medida
+		
+		public void menuPeso()
+		{
+			System.out.println("");
+			System.out.println("1- Miligrama");
+			System.out.println("2- Grama");
+			System.out.println("3- Quilograma");
+			System.out.println("4- Libra");
+			System.out.println("5- Tonelada");
+			System.out.println("");
+		}
+		
+		// Abaixo consta o menu do campo VOLUME contendo as unidades de medida
+
+		public void menuVolume()
+		{
+			System.out.println("");
+			System.out.println("1- Mililitro");
+			System.out.println("2- Litro");
+			System.out.println("3- Milimetros cubicos");
+			System.out.println("4- Centimetros cubicos");
+			System.out.println("5- Decimetros cubicos");
+			System.out.println("6- Metros cubicos");
+			System.out.println("7- Pes cubicos");
+			System.out.println("");
+		}
+		
+		// Abaixo consta o menu do campo TEMPERATURA contendo as unidades de medida
+		
+		public void menuTemperatura()
+		{
+			System.out.println("");
+			System.out.println("1- Celsius");
+			System.out.println("2- Fahrenheit");
+			System.out.println("3- Kelvin");
+			System.out.println("4- Rankine");
+			System.out.println("5- Reaumur");
+			System.out.println("");
+		}
+		
+		// Abaixo consta o menu do campo TEMPO contendo as unidades de medida
+		
+		public void menuTempo()
+		{
+			System.out.println("");
+			System.out.println("1- Milisegundo");
+			System.out.println("2- Segundo");
+			System.out.println("3- Minuto");
+			System.out.println("4- Hora");
+			System.out.println("5- Dia");
+			System.out.println("6- Mes");
+			System.out.println("7- Ano");
+			System.out.println("");
+		}
+		
+		// Abaixo consta o menu do campo VELOCIDADE contendo as unidades de medida
+		
+		public void menuVelocidade()
+		{
+			System.out.println("");
+			System.out.println("1- Pes por segundo");
+			System.out.println("2- Metro por segundo");
+			System.out.println("3- Quilometro por segundo");
+			System.out.println("4- Metro por minuto");
+			System.out.println("5- Quilometro por minuto");
+			System.out.println("6- Quilometro por hora");
+			System.out.println("7- Milha por hora");
+			System.out.println("");
+		}
+		
+		// Abaixo consta o menu do campo VELOCIDADE contendo as unidades de medida
+		
+		public void menuDados()
+		{
+			System.out.println("");
+			System.out.println("1- Bit");
+			System.out.println("2- Byte");
+			System.out.println("3- Kilobyte");
+			System.out.println("4- Kibibyte");
+			System.out.println("5- Kilobit");
+			System.out.println("6- Megabit");
+			System.out.println("7- Gigabit");
+		}
+}
